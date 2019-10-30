711e58e79f629d243ffdef3f307f65796c03dacd
Victor Zuanazzi <victorzuanazzi@gmail.com>
2019-10-30 14:53:47 +0100

    fixed flipped output :wrench:

--- HornSchunck.py
+++ HornSchunck.py
@@ -11,14 +11,19 @@ from matplotlib.pyplot import show
 from argparse import ArgumentParser
 from pyoptflow import HornSchunck, getimgfiles
 from pyoptflow.plots import compareGraphs
+import numpy as np
 
 FILTER = 7
 
 
 def main():
     p = ArgumentParser(description='Pure Python Horn Schunck Optical Flow')
-    p.add_argument('stem', help='path/stem of files to analyze')
-    p.add_argument('pat', help='glob pattern of files', default='*.bmp')
+    p.add_argument('--stem',
+                   help='path/stem of files to analyze',
+                   default='data/box/')
+    p.add_argument('--pat',
+                   help='glob pattern of files',
+                   default='box*.bmp')
     p = p.parse_args()
 
     U, V = horn_schunck(p.stem, p.pat)
@@ -33,10 +38,13 @@ def horn_schunck(stem, pat: str):
         fn1 = flist[i]
         im1 = imageio.imread(fn1, as_gray=True)
 
+        im1 = np.flip(im1, 0)
+
 #       Iold = gaussian_filter(Iold,FILTER)
 
         fn2 = flist[i+1]
         im2 = imageio.imread(fn2, as_gray=True)
+        im2 = np.flip(im2, 0)
 #        Inew = gaussian_filter(Inew,FILTER)
 
         U, V = HornSchunck(im1, im2, 1., 100)
