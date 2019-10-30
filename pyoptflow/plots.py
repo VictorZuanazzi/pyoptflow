2dd519ea4b37db443e9a1e295cdf818d2c51ce0f
Victor Zuanazzi <victorzuanazzi@gmail.com>
2019-10-30 15:29:22 +0100

    Improved quiver plots :sunglasses:

--- pyoptflow/plots.py
+++ pyoptflow/plots.py
@@ -1,5 +1,6 @@
 from matplotlib.pyplot import figure, draw, pause, gca
 from pathlib import Path
+import numpy as np
 
 
 def plotderiv(fx, fy, ft):
@@ -21,17 +22,30 @@ def compareGraphs(u, v, Inew, scale: int = 3, quivstep: int = 5, fn: Path = None
     ax = figure().gca()
     ax.imshow(Inew, cmap='gray', origin='lower')
     # plt.scatter(POI[:,0,1],POI[:,0,0])
-    for i in range(0, u.shape[0], quivstep):
-        for j in range(0, v.shape[1], quivstep):
-            ax.arrow(j, i, v[i, j]*scale, u[i, j]*scale, color='red',
-                     head_width=0.5, head_length=1)
+
+    # downsamples u and v
+    U = u[::quivstep, ::quivstep]
+    V = v[::quivstep, ::quivstep]
+
+    # creates the original X and Y coordinates
+    X = np.zeros(U.shape) + np.arange(0, u.shape[0], quivstep)
+    Y = (np.zeros(U.T.shape) + np.arange(0, u.shape[1], quivstep)).T
+
+    # plot it in one nice step.
+    ax.quiver(X, Y, U, V)
+
+    # for i in range(0, u.shape[0], quivstep):
+    #     for j in range(0, v.shape[1], quivstep):
+    #         ax.arrow(j, i, v[i, j]*scale, u[i, j]*scale, color='red',
+    #                  head_width=0.5, head_length=1)
+
 
         # plt.arrow(POI[:,0,0],POI[:,0,1],0,-5)
     if fn:
         ax.set_title(fn)
 
     draw()
-    pause(0.01)
+    # pause(0.01)
 
 
 def compareGraphsLK(imgOld, imgNew, POI, V, scale=1., fn: Path = None):
