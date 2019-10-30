660c9aeab0259e7eb8920458e779bcdebbedbddf
Victor Zuanazzi <victorzuanazzi@gmail.com>
2019-10-30 15:50:14 +0100

    output image is colorful :ok_hand:

--- HornSchunck.py
+++ HornSchunck.py
@@ -30,24 +30,35 @@ def main():
 
     show()
 
+def rgb2gray(img):
+    """from rgb to gray conversion """
+    if len(img.shape) == 2:
+        # image already is in gray scale
+        return img
+
+    if len(img.shape) > 3:
+        # Not RGB image, just ignore the other channels!
+        img = img[0:3]
+
+    return np.average(img, weights=[0.299, 0.587, 0.114], axis=2)
 
 def horn_schunck(stem, pat: str):
     flist = getimgfiles(stem, pat)
 
     for i in range(len(flist)-1):
         fn1 = flist[i]
-        im1 = imageio.imread(fn1, as_gray=True)
+        im1 = imageio.imread(fn1, as_gray=False)
 
         im1 = np.flip(im1, 0)
 
 #       Iold = gaussian_filter(Iold,FILTER)
 
         fn2 = flist[i+1]
-        im2 = imageio.imread(fn2, as_gray=True)
+        im2 = imageio.imread(fn2, as_gray=False)
         im2 = np.flip(im2, 0)
 #        Inew = gaussian_filter(Inew,FILTER)
 
-        U, V = HornSchunck(im1, im2, 1., 100)
+        U, V = HornSchunck(rgb2gray(im1), rgb2gray(im2), 1., 100)
         compareGraphs(U, V, im2, fn=fn2.name)
 
     return U, V
